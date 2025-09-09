56d0abc390feb7980c0b473c45943ab932a67b67
noppadol.r <riyasarn.n@gmail.com>
2025-09-09 21:40:53 +0700

    modal

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Projects\Index;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -19,6 +20,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+
+    Route::get('projects', Index::class)->name('projects');
 });
 
 require __DIR__.'/auth.php';
